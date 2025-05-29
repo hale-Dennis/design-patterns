1cdbda4179a737eadf3f628c8544415996427475
Dennis Owusu <haledennis0@gmail.com>
2025-05-29 11:25:09 +0000

    feat: Add Open Close Principle

--- SOLID Principles/src/Main.java
+++ SOLID Principles/src/Main.java
@@ -1,12 +1,16 @@
 import srp.User;
+import ocp.Notification;
 import srp.UserRepository;
+import ocp.SMSNotification;
+import ocp.EmailNotification;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        // SRP Test
-        User user = new User("John Doe", "johndoe@example.com");
-        UserRepository userRepo = new UserRepository();
-        userRepo.save(user);
+        Notification email = new EmailNotification();
+        Notification sms = new SMSNotification();
+        email.send("Hello via Email!");
+        sms.send("Hello via SMS!");
     }
 }
